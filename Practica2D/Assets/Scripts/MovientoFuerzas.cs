5a8066af86b6fc9a668774faf7a094baf8035698
alepagu <padilla.guale22@cordoba.salesianos.edu>
2024-02-09 17:46:42 +0100

    APG 09/02/2024
    Scripts y Animaciones

---
+++ Practica2D/Assets/Scripts/MovientoFuerzas.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovientoFuerzas : MonoBehaviour
+{
+    public float speed;
+    float inputMovement;
+    Rigidbody2D rigidBody2D;
+    public bool isLookingRight;
+    public float jumpSpeed;
+
+    //Arregle Jump
+    BoxCollider2D boxCollider;
+    public bool isOnFloor;
+    public LayerMask surfaceLayer;
+
+    //Transition Animations
+    public bool isRunning;
+    private Animator animator;
+
+    private void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    bool ChekingFloor()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(
+                                    boxCollider.bounds.center, //Origen de la caja
+                                    new Vector2(boxCollider.bounds.size.x, boxCollider.bounds.size.y), //Tamaño, por defecto 1
+                                    0f, //Ángulo
+                                    Vector2.down, //Direccion hacia la que va la caja
+                                    0.2f, //Distancia a la que aparece la caja
+                                    surfaceLayer//Layer mask
+                                    );
+
+        return raycastHit.collider != null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessingMovement();
+        isOnFloor = ChekingFloor();
+        ProcessingJump();
+    }
+
+    //Función para las fuerzas
+    void ProcessingMovement () 
+    {
+        inputMovement = Input.GetAxis("Horizontal");
+        isRunning = inputMovement != 0 ? true : false;
+        animator.SetBool("isRunning", isRunning);
+        rigidBody2D.velocity = new Vector2(speed * inputMovement, rigidBody2D.velocity.y);
+        CharacterHOrientation(inputMovement);
+    }
+
+    private void ProcessingJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnFloor)
+        {
+            rigidBody2D.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+        }
+    }
+
+    private void CharacterHOrientation(float inputMovement)
+    {
+        if ((isLookingRight && inputMovement < 0) || (!isLookingRight && inputMovement > 0))
+        {
+            isLookingRight = !isLookingRight;
+            transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+        }
+
+    }
+}
