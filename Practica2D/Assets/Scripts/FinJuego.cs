d809084d7ff885f90d2bf8377d23804a1d370e2c
alepagu <padilla.guale22@cordoba.salesianos.edu>
2024-02-16 13:32:41 +0100

    16/02/24
    Enemies and modificated scripts

---
+++ Practica2D/Assets/Scripts/FinJuego.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinJuego : MonoBehaviour
+{
+    // Referencia al objeto que finaliza la partida
+    public GameObject trofeo;
+
+    // Método que se llama cuando otro collider entra en contacto con el collider de este objeto
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            Debug.Log("El personaje ha logrado llegar a meta. ¡Has terminado la partida!");
+            Destroy(this.gameObject);
+            Application.Quit(); //Salir de la partida.
+        }
+
+    }
+}
