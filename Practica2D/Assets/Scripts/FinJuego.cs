e85c05092c0a3ed62b6d78bccac63bffe0bf9a3e
alepagu <padilla.guale22@cordoba.salesianos.edu>
2024-03-01 17:29:03 +0100

    APG 01/03/2024
    Finalización del juego SigueVivo_SigueRitmo que es la entrega de la Práctica8.

--- Practica2D/Assets/Scripts/FinJuego.cs
+++ Practica2D/Assets/Scripts/FinJuego.cs
@@ -7,20 +7,24 @@ public class FinJuego : MonoBehaviour
     // Referencia al objeto que finaliza la partida
     public GameObject trofeo;
 
+    //Tiempos para finalizar juego
+    private float tiempoLimite = 16f; // Tiempo límite en segundos
+    private float tiempoTranscurrido = 0f;
+    private bool terminarPartida = false;
+
     //Sonido final
     public AudioClip winner;
 
     // Método que se llama cuando otro collider entra en contacto con el collider de este objeto
-        private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D other)
         {
-            Debug.Log("El personaje ha logrado llegar a meta.");
-            Debug.Log("¡Has terminado la partida!");
-            Destroy(this.gameObject);
-        AudioManager.Instance.ReproducirSonido(winner);
-            Application.Quit();
-            
-            
-            
-        }
+            if (other.CompareTag("Player"))
+            {
+                Debug.Log("¡Has terminado la partida!");
+                Destroy(this.gameObject);
+                AudioManager.Instance.ReproducirSonido(winner);
+                
+            }
+    }
 
 }
